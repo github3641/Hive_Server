059c92d5e74817af081e4021e0a97b5ed51f4528
吕瑞超 <190654@lifeat.cn>
2020-08-20 19:35:58 +0800

    优化

--- dc_srv/src/main/java/org/example/dc/srv/service/HiveQuery.java
+++ dc_srv/src/main/java/org/example/dc/srv/service/HiveQuery.java
@@ -1,6 +1,8 @@
 package org.example.dc.srv.service;
 
 import com.alibaba.fastjson.JSONObject;
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import com.sun.tools.corba.se.idl.StringGen;
 import org.example.dc.srv.api.HiveQueryService;
 import org.example.dc.srv.enums.ExecutionStatusEnum;
 import org.example.dc.srv.utils.ComposeSqlUtil;
@@ -26,14 +28,6 @@ import java.util.Map;
  */
 public class HiveQuery implements HiveQueryService{
     private static final Logger logger = LoggerFactory.getLogger(HiveQuery.class);
-    private static final String QUERY_MODE="queryMode";//查询模式
-    private static final String CUSTOM_MODE="customQuery";//自定义模式
-    private static final String STANDARD_MODE="standardQuery";//标准模式
-    private static final String QUERY_SQL="querySql";//查询SQL
-    public static final String COLUMNS="COLUMNS";
-    public static final String PARTCOLUMN="PARTCOLUMN";
-    public static final String TABLENAME="TABLENAME";
-    public static final String OTHER_PARAMETER="OTHER_PARAMETER";
 
     /**
      * 方法说明:根据传入参数，查询hive数仓，
@@ -46,19 +40,36 @@ public class HiveQuery implements HiveQueryService{
     public Map<String,String> qureyDataToJson(Map<String,String> map) throws SQLException, IOException {
 
         Map<String,String> result = new HashMap();
+        //创建写入文件的流
+        String pathName= "D:\\WorkSpace\\Hive_Server\\dc_srv\\src\\main\\resources\\test.txt";
+        File file = new File(pathName);
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter  bw = new BufferedWriter(fw);
+
+        //初始化结果集
+        result.put("filePath",file.getAbsolutePath());
+        result.put("executionStatus",ExecutionStatusEnum.FAILED.getMsg());
+
+        //校验传入参数
+        String sql = parameterParsing(map);
+
+        //创建连接
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = HiveJDBCUtil.getConn();
+            stmt = HiveJDBCUtil.getStmt(conn);
+        } catch (Exception e) {
+            throw new RuntimeException("获取连接失败!");
+        }
 
         //获得查询结果
-        ResultSet resultSet = getResultSet(map);
+        ResultSet resultSet = getResultSet(stmt,sql);
+
 
         //将结果数据转成json保存到文件
-        ResultSetMetaData meta = resultSet.getMetaData();//获取字段
-       String pathName= "D:\\WorkSpace\\Hive_Server\\dc_srv\\src\\main\\resources\\test.txt";
-        File file = null;
-        BufferedWriter bw = null;
         try {
-            file = new File(pathName);
-            FileWriter fw = new FileWriter(file);
-            bw = new BufferedWriter(fw);
+            ResultSetMetaData meta = resultSet.getMetaData();
             while(resultSet.next()) {
                 JSONObject obj = new JSONObject();
                 for(int i=1;i<=meta.getColumnCount();i++) {
@@ -68,18 +79,18 @@ public class HiveQuery implements HiveQueryService{
                 bw.write(str);
                 bw.newLine();
             }
+
+            result.put("executionStatus",ExecutionStatusEnum.SUCCESS.getMsg());
         } catch (Exception e) {
-            System.out.println("写入文件失败");
-            result.put("filePath",file.getAbsolutePath());
-            result.put("executionStatus",ExecutionStatusEnum.FAILED.getMsg());
+            throw  new RuntimeException("写入文件失败!");
+        }finally {
+            conn.close();
+            bw.close();
         }
-        bw.close();
-        result.put("filePath",file.getAbsolutePath());
-        result.put("executionStatus",ExecutionStatusEnum.SUCCESS.getMsg());
+
         //返回结果
         return result;
     }
-
 
     /***
      * 方法说明：
@@ -95,8 +106,8 @@ public class HiveQuery implements HiveQueryService{
         Map resultMap = new HashMap();
         //解析传入参数
         logger.info("开始解析传入参数");
-        String queryMode = map.get(QUERY_MODE);
-        String querySql = map.get(QUERY_SQL);
+        String queryMode = map.get("queryMode");
+        String querySql = map.get("querySql");
         //获取hive连接
         Connection connection = null;
         Statement statement = null;
@@ -109,12 +120,12 @@ public class HiveQuery implements HiveQueryService{
         //判断所需参数是否为null
         if(queryMode == null){
             throw new RuntimeException("请输入查询模式参数");
-        }else if(CUSTOM_MODE.equals(queryMode) && querySql == null){
+        }else if("customQuery".equals(queryMode) && querySql == null){
             throw new RuntimeException("自定义模式下，请输入查询SQL");
         }
 
         //根据传入的查询模式来判断sql是否需要拼接
-        if(CUSTOM_MODE.equals(queryMode)){
+        if("customQuery".equals(queryMode)){
             //自定义查询模式
             ResultSet resultSet = null;
             try {
@@ -130,19 +141,19 @@ public class HiveQuery implements HiveQueryService{
                 e.printStackTrace();
             }
             //sql拼接方式
-        }else if(STANDARD_MODE.equals(queryMode)){
+        }else if("standardQuery".equals(queryMode)){
 
             //读取拼接sql所用参数
-            String columns = map.get(COLUMNS);
-            String partColumn = map.get(PARTCOLUMN);
-            String tableName = map.get(TABLENAME);
-            String other_parameter = map.get(OTHER_PARAMETER);
+            String "columns" = map.get("columns");
+            String "partcolumn" = map.get("partcolumn");
+            String "tableName" = map.get("tableName");
+            String "otherParameter" = map.get("otherParameter");
             //传入参数，产出拼接好的SQL
             Map mapToSql = new HashMap();
-            mapToSql.put(COLUMNS,columns);
-            mapToSql.put(PARTCOLUMN,partColumn);
-            mapToSql.put(TABLENAME,tableName);
-            mapToSql.put(OTHER_PARAMETER,other_parameter);
+            mapToSql.put("columns","columns");
+            mapToSql.put("partcolumn","partcolumn");
+            mapToSql.put("tableName","tableName");
+            mapToSql.put("otherParameter","otherParameter");
             String sql = ComposeSqlUtil.getSql(mapToSql);
             //标准查询模式
             ResultSet resultSet = null;
@@ -168,47 +179,62 @@ public class HiveQuery implements HiveQueryService{
         return resultMap;
     }
 
-    public ResultSet getResultSet(Map<String,String> map){
-        //解析传入参数
-        logger.info("开始解析传入参数");
-        String queryMode = map.get(QUERY_MODE);
-        String querySql = map.get(QUERY_SQL);
-        //获取hive连接
-        Connection conn = null;
-        Statement stmt = null;
+    /**
+     * 方法说明:传入查询sql及连接，返回hive查询结果
+     * @return
+     */
+    public ResultSet getResultSet(Statement stmt,String sql){
 
-        try {
-            conn = HiveJDBCUtil.getConn();
-            stmt = HiveJDBCUtil.getStmt(conn);
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }catch (SQLException e){
-            e.printStackTrace();
-        }
-
-        //判断所需参数是否为null
-        if(queryMode == null){
-            throw new RuntimeException("请输入查询模式参数");
-        }else if(CUSTOM_MODE.equals(queryMode) && querySql == null){
-            throw new RuntimeException("自定义模式下，请输入查询SQL");
-        }
-
-        //根据传入参数查询，并写入文件
+        //根据传入sql查询，并写入文件
         ResultSet resultSet=null;
-        if (CUSTOM_MODE.equals(queryMode)){
-            //根据参数查询数据
             try {
-                resultSet= stmt.executeQuery(querySql);//返回执行结果集
-                conn.close();
+                resultSet= stmt.executeQuery(sql);//返回执行结果集
+
             } catch (SQLException e) {
-                e.printStackTrace();
+                throw new RuntimeException("查询数据失败!");
             }
 
-        }else if(STANDARD_MODE.equals(queryMode)){
-
-        }else{
-            throw new RuntimeException("查询模式参数异常");
-        }
         return  resultSet;
+    }
+
+    /**
+     * 方法说明:解析并校验传入的参数,返回查询sql
+     * @param map
+     * @return
+     */
+    public String parameterParsing (Map<String,String> map){
+
+        //方法返回的查询sql
+        String querySql=null;
+
+        logger.info("开始解析校验传入参数");
+        //解析传入参数
+        String queryMode = map.get("queryMode");
+        String sql = map.get("querySql");
+        String columns =map.get("columns");
+        String partcolumn=map.get("partcolumn");
+        String tableName=map.get("tableName");
+        String otherParameter=map.get("otherParameter");
+
+        //校验传入参数
+        if(queryMode == null){
+            throw new RuntimeException("请输入查询模式参数:queryMode");
+        }else if(!"customQuery".equals(queryMode) && !"standardQuery".equals(queryMode)){
+            throw new RuntimeException("请正确输入查询模式参数:queryMode (customQuery|standardQuery)");
+        }
+
+        if ("customQuery".equals(queryMode)&& sql!=null){
+            querySql=sql;
+        }else if ("customQuery".equals(queryMode)&& sql==null){
+            throw new RuntimeException("选择自定义查询模式下，请正确传入自定义sql!");
+        }else if("standardQuery".equals(queryMode)&& "tableName"!=null){
+            String sql2 = ComposeSqlUtil.getSql(map);
+            querySql=sql2;
+        }else{
+            throw new RuntimeException("选择标准查询模式下，请传入库名和表名!");
+        }
+
+        logger.info("解析校验传入参数结束");
+        return querySql;
     }
 }
